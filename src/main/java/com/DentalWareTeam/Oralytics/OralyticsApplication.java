449bad1f7abcf7898a522593b1b9ea8ba0647240
kayquesan2 <rm552605@fiap.com.br>
2024-09-28 18:34:55 -0300

    initialization

---
+++ src/main/java/com/DentalWareTeam/Oralytics/OralyticsApplication.java
@@ -0,0 +1,13 @@
+package com.DentalWareTeam.Oralytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OralyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OralyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DentalWareTeam/Oralytics/OralyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DentalWareTeam.Oralytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OralyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
