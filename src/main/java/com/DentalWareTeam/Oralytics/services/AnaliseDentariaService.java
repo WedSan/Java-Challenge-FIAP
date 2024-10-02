221dc4f8747c72f705ee3ef7095848ce46227b67
kayquesann <kayque.ferreirasan@gmail.com>
2024-10-02 18:38:23 -0300

    Criação dos métodos da classe AnaliseDentariaService

--- src/main/java/com/DentalWareTeam/Oralytics/services/AnaliseDentariaService.java
+++ src/main/java/com/DentalWareTeam/Oralytics/services/AnaliseDentariaService.java
@@ -1,4 +1,46 @@
 package com.DentalWareTeam.Oralytics.services;
 
+import com.DentalWareTeam.Oralytics.dto.AnaliseDentariaDTO;
+import com.DentalWareTeam.Oralytics.model.AnaliseDentaria;
+import com.DentalWareTeam.Oralytics.repositories.AnaliseDentariaRepository;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class AnaliseDentariaService {
+
+    @Autowired
+    private AnaliseDentariaRepository analiseDentariaRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public AnaliseDentariaService(AnaliseDentariaRepository analiseDentariaRepository, ModelMapper modelMapper) {
+        this.analiseDentariaRepository = analiseDentariaRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public List<AnaliseDentariaDTO> listarAnalisesDentarias() {
+        List<AnaliseDentaria> analises = analiseDentariaRepository.findAll();
+        return analises.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public AnaliseDentariaDTO salvarAnaliseDentaria(@Valid AnaliseDentariaDTO analiseDentariaDTO) {
+        AnaliseDentaria analise = convertToEntity(analiseDentariaDTO);
+        AnaliseDentaria analiseSalva = analiseDentariaRepository.save(analise);
+        return convertToDTO(analiseSalva);
+    }
+
+    private AnaliseDentariaDTO convertToDTO(AnaliseDentaria analiseDentaria) {
+        return modelMapper.map(analiseDentaria, AnaliseDentariaDTO.class);
+    }
+
+    private AnaliseDentaria convertToEntity(AnaliseDentariaDTO analiseDentariaDTO) {
+        return modelMapper.map(analiseDentariaDTO, AnaliseDentaria.class);
+    }
 }
