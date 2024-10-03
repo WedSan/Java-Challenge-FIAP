af861e77ef6e91f151217b5223f66a973e07750e
kayquesann <kayque.ferreirasan@gmail.com>
2024-10-03 19:49:50 -0300

    Criação dos métodos da classe GlobalExceptionHandler

--- src/main/java/com/DentalWareTeam/Oralytics/GlobalExceptionHandler.java
+++ src/main/java/com/DentalWareTeam/Oralytics/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.DentalWareTeam.Oralytics;
 
-import com.DentalWareTeam.Oralytics.exceptions.UsuarioNotFoundException;
+import com.DentalWareTeam.Oralytics.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +13,20 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
+    public ResponseEntity<String> handleDadoMonitoramentoNotFoundException (DadoMonitoramentoNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
 
+    public ResponseEntity<String> handleHistoricoDentalNotFoundException (HistoricoDentalNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    public ResponseEntity<String> handleRelatoProblemaDentario (RelatoProblemaDentarioNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    public ResponseEntity<String> handleAnaliseDentaria (AnaliseDentariaNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
 
 }
