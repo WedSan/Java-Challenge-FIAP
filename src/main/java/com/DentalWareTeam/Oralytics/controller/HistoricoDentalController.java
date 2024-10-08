19b7ba3f3b2455f3ba1c73ec4cce8b2b7e124127
WedSan <andre.alves.silvazesez@gmail.com>
2024-10-07 22:20:48 -0300

    adiciona metodo para apagar historico dental por id

--- src/main/java/com/DentalWareTeam/Oralytics/controller/HistoricoDentalController.java
+++ src/main/java/com/DentalWareTeam/Oralytics/controller/HistoricoDentalController.java
@@ -1,25 +1,29 @@
 package com.DentalWareTeam.Oralytics.controller;
 
+import com.DentalWareTeam.Oralytics.dto.AdicionarHistoricoDentalDTO;
 import com.DentalWareTeam.Oralytics.dto.HistoricoDentalDTO;
+import com.DentalWareTeam.Oralytics.mapper.HistoricoDentalMapper;
+import com.DentalWareTeam.Oralytics.model.HistoricoDental;
 import com.DentalWareTeam.Oralytics.services.HistoricoDentalService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/historicoDental")
+@RequestMapping("/historico-dental")
 public class HistoricoDentalController {
 
     @Autowired
     private HistoricoDentalService historicoDentalService;
 
     @PostMapping
-    public ResponseEntity<HistoricoDentalDTO> adicionarHistoricoDental (@RequestBody @Valid HistoricoDentalDTO historicoDentalDTO){
+    public ResponseEntity<HistoricoDentalDTO> adicionarHistoricoDental (@RequestBody @Valid AdicionarHistoricoDentalDTO historicoDentalDTO){
         HistoricoDentalDTO historico = historicoDentalService.salvarHistoricoDental(historicoDentalDTO);
-        return ResponseEntity.ok(historico);
+        return ResponseEntity.status(HttpStatus.CREATED).body(historico);
     }
 
     @GetMapping
@@ -30,7 +34,13 @@ public class HistoricoDentalController {
 
     @GetMapping("/{id}")
     public ResponseEntity<HistoricoDentalDTO> obterHistoricoDental (@PathVariable Integer id) {
-        HistoricoDentalDTO historico = historicoDentalService.lerHistoricoDental(id);
-        return ResponseEntity.ok(historico);
+        HistoricoDental historico = historicoDentalService.lerHistoricoDental(id);
+        return ResponseEntity.ok(HistoricoDentalMapper.toDTO(historico));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarHistoricoDental(@PathVariable Integer id){
+        historicoDentalService.deletarHistoricoDental(id);
+        return ResponseEntity.noContent().build();
     }
 }
